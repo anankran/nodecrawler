5ac73ded76de3c137dbeda5e26e6fadd90aae305
André Nankran <nankran@inventati.org>
2017-05-03 15:11:44 -0300

    new push

--- hacking.js
+++ hacking.js
@@ -1,11 +1,11 @@
 var Crawler = require("crawler");
-var crawler = new Crawler({ maxConnections : 10 });
 
-module.exports = function (ctx, done){
+module.exports = function(callback){
+    var crawler = new Crawler({ maxConnections : 10 });
     for(x = 0; x <= 10; x++){
         page = x * 10;
         crawler.queue([{
-            uri: 'https://www.indeed.com/jobs?q=node.js&start='+page,
+            uri: 'https://www.indeed.com/jobs?q=node.js&start=0',
             jQuery: true,
             callback: function (error, res, done) {
                 if(error){
@@ -13,21 +13,20 @@ module.exports = function (ctx, done){
                 } else {
                     var $ = res.$;
                     var company,job,link;
-                    var arr = [];
-                    var json = '{';
-                    $('.result').each(function(key,value){
-                        row = $(this).find('.jobtitle a');
-                        job = row.text().trim();
-                        link = row.attr('href');
-                        company = $(this).find('.company span').text().trim();
-                        arr[key] = ' { job : "'+job+'", link : "https://www.indeed.com'+link+'", company : "'+company+'" }';
-                    });
-                    json += arr.join(',');
-                    json += ' }';
-                    //const json = json;
-                    return json;
+                    var json = [];
+                    if($('#resultsCol .result').length > 0){
+                        $('#resultsCol .result').each(function(key,value){
+                            row = $(this).find('.jobtitle a');
+                            job = row.text().trim();
+                            link = row.attr('href');
+                            company = $(this).find('.company span').text().trim();
+                            json[key] = { job : job, link : 'https://www.indeed.com'+link, company : company };
+                        });
+                        console.log(json);
+                        callback(null, json);
+                    }
                 }
-                done(json, 'Success.');
+                done();
             }
         }]);
     }
